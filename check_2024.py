0078f50a63d606384ecb28be2998523fe2bac225
SarangPratap <chamola31@gmail.com>
2026-02-04 15:14:13 +0100

    Remove temporary test files - clean up branch

--- check_2024.py
+++
@@ -1,7 +0,0 @@
-from data_engine import get_race_weekends_by_year, enable_cache
-enable_cache()
-events_2024 = get_race_weekends_by_year(2024)
-sprint_events = [e for e in events_2024 if 'sprint' in e['type'].lower()]
-print(f'2024 Sprint Events: {len(sprint_events)}')
-for e in sprint_events[:5]:
-    print(f'  Round {e["round_number"]}: {e["event_name"]}')
